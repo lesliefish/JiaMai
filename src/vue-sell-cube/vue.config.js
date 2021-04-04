33e79df716d6c0d4089117d9e55b42fb9e3151d7
leiyu <yuleics@outlook.com>
2021-04-04 14:19:57 +0800

    add mock serve config

--- src/vue-sell-cube/vue.config.js
+++ src/vue-sell-cube/vue.config.js
@@ -1,18 +1,38 @@
+const appData = require("./data.json")
+const seller = appData.seller;
+const goods = appData.goods;
+
 module.exports = {
-  css: {
-    loaderOptions: {
-      stylus: {
-        'resolve url': true,
-        'import': [
-          './src/theme'
-        ]
-      }
+    css: {
+        loaderOptions: {
+            stylus: {
+                'resolve url': true,
+                'import': [
+                    './src/theme'
+                ]
+            }
+        }
+    },
+    pluginOptions: {
+        'cube-ui': {
+            postCompile: true,
+            theme: true
+        }
+    },
+    devServer: {
+        before(app) {
+            app.get('/api/seller', function (req, res) {
+                res.json({
+                    errno: 0,
+                    data: seller
+                });
+            }),
+                app.get('/api/goods', function (req, res) {
+                    res.json({
+                        errno: 0,
+                        data: goods
+                    });
+                })
+        }
     }
-  },
-  pluginOptions: {
-    'cube-ui': {
-      postCompile: true,
-      theme: true
-    }
-  }
 }
